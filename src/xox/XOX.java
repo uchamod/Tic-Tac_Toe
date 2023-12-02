4a4110e141951b0a09c055e53fe6e3390344af71
Chamod Udara <128582580+uchamod@users.noreply.github.com>
2023-12-02 23:36:46 +0530

    Initial commit

---
+++ src/xox/XOX.java
@@ -0,0 +1,14 @@
+
+package xox;
+
+
+public class XOX {
+
+  
+    public static void main(String[] args) {
+        xoxframe sos=new xoxframe();
+        sos.setVisible(true);
+        sos.setLocationRelativeTo(null);
+    }
+    
+}
